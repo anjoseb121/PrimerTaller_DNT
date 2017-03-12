75751680828f616e45e06d998774ff87cc05dc0f
AntonioBar <anjose.121@gmail.com>
2017-03-11 22:36:35 -0500

    Initial commit. Added MainActivity with list of activities.

---
+++ app/src/main/java/com/ajbe/primertaller/adapter/ActivitiesAdapter.java
@@ -0,0 +1,54 @@
+package com.ajbe.primertaller.adapter;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.ajbe.primertaller.R;
+
+/**
+ * Created by HP on 3/11/2017.
+ */
+public class ActivitiesAdapter extends RecyclerView.Adapter<ActivitiesAdapter.ViewHolder> {
+
+    private final TypedArray mImages;
+
+    public ActivitiesAdapter(Context context) {
+        mImages = context.getResources().obtainTypedArray(R.array.icons);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_recycler_activities, parent, false);
+        // Return a new holder instance
+        return new ViewHolder(contactView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.imageView.setImageDrawable(mImages.getDrawable(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.length();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            imageView = (ImageView) itemView.findViewById(R.id.item_image_icon);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ajbe/primertaller/presentation/MainActivity.java
@@ -0,0 +1,55 @@
+package com.ajbe.primertaller.presentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ajbe.primertaller.R;
+import com.ajbe.primertaller.adapter.ActivitiesAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_activities);
+        ActivitiesAdapter adapter = new ActivitiesAdapter(this);
+        mRecyclerView.setAdapter(adapter);
+        StaggeredGridLayoutManager gridLayoutManager =
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
